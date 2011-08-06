ee2f94b9d2380485998848f19d14f22bae1d8694
Emmanuel Roullit <emmanuel@netsniff-ng.org>
2011-08-06 22:35:49 +0200

    Make sure the string given to /proc does not have trailing \n

--- otst.c
+++ otst.c
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * cat /proc/driver/otst ... to show currently traced syms
- * echo "netif_rx" > /proc/driver/otst ... to add a symbol for trace
+ * echo -n "netif_rx" > /proc/driver/otst ... to add a symbol for trace
  */
 
 #include <linux/kernel.h>
